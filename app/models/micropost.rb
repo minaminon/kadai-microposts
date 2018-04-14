28957e962ce9cf72d416848ef9b5209e15a62ab2
naoto.mnmn <houganhalf@yahoo.co.jp>
2018-04-14 15:29:54 +0000

    add dependent destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   has_many :favorited ,class_name: 'Favorite',foreign_key: 'micropost_id'
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   
   validates :user_id, presence: true
   validates :content, presence: true, length:{maximum: 255}
